105d9452431e05d8927eeeb0d579ebf11c51ebe7
rioshen <rioxshen@gmail.com>
2015-03-10 23:17:11 -0400

    Simply point is wrong, as we also hope the result is sored too.

--- exercises/src/main/java/Chapter3/ListUion.java
+++ exercises/src/main/java/Chapter3/ListUion.java
@@ -22,12 +22,20 @@ public class ListUion {
             return l1;
         }
 
-        ListNode tail = l1;
-        while (tail.next != null) {
-            tail = tail.next;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
         }
-        tail.next = l2;
 
-        return l1;
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
     }
 }
